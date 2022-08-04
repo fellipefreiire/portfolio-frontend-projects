f546580b99053b7c54b63ec816bfe9d7f6d0d623
Felipe <fellipefreiire3@gmail.com>
2022-08-03 23:22:31 -0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import './styles/global/styles.css'
-import { Sidebar } from 'home/Sidebar'
 import { ChakraProvider } from '@chakra-ui/react'
 
 export const App = () => {
   return (
     <ChakraProvider>
-      <div>
-        <Sidebar />
-      </div>
+      <div></div>
     </ChakraProvider>
   )
 }
