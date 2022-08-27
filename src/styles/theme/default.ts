60b003bc01265a68b9eea0047ad6bdd90876cb2a
Felipe <fellipefreiire3@gmail.com>
2022-08-27 13:56:51 -0300

    theme fix

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -1,4 +1,9 @@
-export const theme = {
+import { extendTheme } from '@chakra-ui/react'
+
+// 2. Add your color mode config
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: true,
   styles: {
     global: {
       '*': {
@@ -11,3 +16,6 @@ export const theme = {
     breakpoints: ['37.4375em', '37.5em', '56.25em', '75em', '112.5em'],
   },
 }
+
+// 3. extend the theme
+export const theme = extendTheme({ config })
